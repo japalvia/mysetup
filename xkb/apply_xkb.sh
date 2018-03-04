afe2925d9f342f68dc5dce13f0ff68716ec401f9
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2018-03-04 21:31:04 +0200

    Handle bad usage.

--- xkb/apply_xkb.sh
+++ xkb/apply_xkb.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+usage() {
+    cat >&2 <<EOF
+xkb loader usage:
+$0 [directory]
+    where directory is the location for kxb layout files
+    (i.e. directory must contain 'symbols' subdirectory)
+EOF
+}
+
+bad_usage() {
+    printf 'ERROR: %s\n' "$1" >&2
+    usage
+    exit 1
+}
+
 dir=$1
+[ -z "$dir" ] && bad_usage "Directory argument missing"
+[ ! -d "$dir" ] && bad_usage "Does not exist or is not a directory: $dir"
 
 setxkbmap -layout fi
 setxkbmap -option ctrl:nocaps
-# Note: no whitespace allowed after option flag
 setxkbmap -I"$dir" prog -print | xkbcomp -I"$dir" - $DISPLAY
