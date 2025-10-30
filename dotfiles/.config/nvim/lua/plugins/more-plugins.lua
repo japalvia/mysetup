16fb2e9370b9b82d8cfaf1defab3466f595db0f2
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2025-10-30 16:33:15 +0200

    lazyvim: configure clangd query driver

--- dotfiles/.config/nvim/lua/plugins/more-plugins.lua
+++ dotfiles/.config/nvim/lua/plugins/more-plugins.lua
@@ -35,4 +35,24 @@ return {
       },
     },
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+            "--query-driver=/home/jarkko/dev/**/aarch64-helix-linux-g++,/home/jarkko/dev/**/arm-helix-linux-gnueabi*,/usr/bin/*",
+          },
+        },
+      },
+    },
+  },
 }
