835e8ff2f4122c5bf890a00b617497cb5a861929
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2025-12-14 17:02:37 +0200

    lazyvim: disable autoformatting for linux repo

--- dotfiles/.config/nvim/lua/config/autocmds.lua
+++ dotfiles/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local set_autoformat = function(pattern, bool_val)
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = pattern,
+    callback = function()
+      vim.b.autoformat = bool_val
+    end,
+  })
+end
+
+set_autoformat("*/linux/*", false)
