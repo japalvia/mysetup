31d2c007ed535efdbdf8438f701fc106f4f68f6b
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2026-05-03 17:02:56 +0300

    lazyvim: rst preview plugin

---
+++ dotfiles/.config/nvim/lua/plugins/rst.lua
@@ -0,0 +1,10 @@
+return {
+  "brianhuster/live-preview.nvim",
+  dependencies = {
+    -- You can choose one of the following pickers
+    -- "nvim-telescope/telescope.nvim",
+    -- "ibhagwan/fzf-lua",
+    -- "nvim-mini/mini.pick",
+    "folke/snacks.nvim",
+  },
+}
