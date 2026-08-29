a8732f2212104a22be017430b41a41b62d38dbe0
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2026-08-29 22:35:21 +0300

    swayimg: config

--- dotfiles/.config/swayimg/init.lua
+++ dotfiles/.config/swayimg/init.lua
@@ -4,5 +4,21 @@ swayimg.enable_overlay(true)
 -- Set background color for transparent images (white)
 swayimg.viewer.set_image_background(0xffffffff)
 
+-- Scale images to fit available window space on open
+swayimg.viewer.set_default_scale("fit")
+
 -- Disable top-left text block in viewer mode
 swayimg.viewer.set_text("topleft", {})
+
+-- Bind q to quit in all modes
+swayimg.viewer.on_key("q", function()
+  swayimg.exit()
+end)
+
+swayimg.slideshow.on_key("q", function()
+  swayimg.exit()
+end)
+
+swayimg.gallery.on_key("q", function()
+  swayimg.exit()
+end)
